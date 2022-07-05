54ea01dd65ffa1253b2e7f86b8eed9a680a09d44
deun_0124 <m3g3ab@gmail.com>
2022-07-05 18:09:39 +0900

    Redux로그인페이지

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,13 +2,20 @@ import React, { useEffect } from 'react'
 import axios from 'axios';
 
 function LandingPage() {
+
+
     useEffect(()=>{
         axios.get('api/hello')
         .then(response => console.log(response.data))
     },[])
 
   return (
-    <div>LandingPage</div>
+    <div style={{
+      display:'flex', justifyContent:'center',alignItems:'center'
+      ,width:'100%',height:'100vh'
+    }}>
+      <h2>시작페이지</h2>
+    </div>
   )
 }
 
